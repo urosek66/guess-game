489eeec786a53fe783c4598b7120a9b2e3b297e0
Uroš Rojnik <97902369+urosek66@users.noreply.github.com>
2022-09-03 21:53:17 +0200

    some changes to the game

--- main.py
+++ main.py
@@ -8,30 +8,36 @@ app = Flask(__name__)
 def index():
     if request.method == "GET":
 
-        secret_number = request.cookies.get("number")
+        secret_number = request.cookies.get("secret_number")
 
+        response = make_response(render_template("index.html"))
         if not secret_number:
-            secret_number = random.randint(1, 30)
-            response = make_response(render_template("index.html"))
-            response.set_cookie("secret_num", str(secret_number))
+            random_number = random.randint(1, 30)
+            response.set_cookie("secret_number", str(random_number))
 
-            return response
+        return response
 
     elif request.method == "POST":
-        secret_number = request.cookies.get("secret_num")
-        user_guess = request.form.get("number")
+        secret_number = int(request.cookies.get("secret_number"))
+        user_guess = int((request.form.get("number")))
 
         if secret_number == user_guess:
             text = "Congratulations your are correct."
-            return render_template("result.html", text=text)
+            play_again = "Start over"
+            response = make_response(render_template("result.html", text=text, play_again=play_again))
+            response.set_cookie("secret_number", str(random.randint(1, 30)))
+
+            return response
 
         elif secret_number > user_guess:
             text = "Sorry your guess is to small."
-            return render_template("result.html", text=text)
+            play_again = "Play again"
+            return render_template("result.html", text=text, play_again=play_again)
 
         else:
             text = "Sorry your guess is to big."
-            return render_template("result.html", text=text)
+            play_again = "Play again"
+            return render_template("result.html", text=text, play_again=play_again)
 
 
 if __name__ == '__main__':
