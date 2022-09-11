37d38aa016547c8be1b297e5892e07e12e302b89
Uroš Rojnik <97902369+urosek66@users.noreply.github.com>
2022-09-11 19:24:55 +0200

    change

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 import os
 from sqla_wrapper import SQLAlchemy
 
-db = SQLAlchemy(os.getenv("DATABSE_URL", "sqlite:///localhost.sqlite"))
+db_url = os.getenv("DATABSE_URL", "sqlite:///localhost.sqlite").replace("postgres://", "postgresl://", 1)
+db = SQLAlchemy(db_url)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
