58b003a3daffa13a0b1b2331ba85a3409ec21dad
Uroš Rojnik <97902369+urosek66@users.noreply.github.com>
2022-09-12 16:29:52 +0200

    code changes

--- main.py
+++ main.py
@@ -20,26 +20,25 @@ def index():
         user = User(name=name, email=email, secret_number=secret_number)
         user.save()
 
-        response = make_response(redirect(url_for('game')))
-        response.set_cookie("user_name", name)
+        response = make_response(render_template("game.html"))
+        response.set_cookie("user_email", email)
 
         return response
 
 
 @app.route("/game", methods=["GET", "POST"])
 def game():
-    if request.method == "GET":
 
+    if request.method == "GET":
 
         return render_template("game.html")
 
     elif request.method == "POST":
-        user_guess = request.form.get("number")
-        user_name = request.cookies.get("user_name")
-        user = db.query(User).filter_by(name=user_name).first()
-        secret_number = user.secret_number
+        user_guess = int(request.form.get("number"))
+        user_email = request.cookies.get("user_email")
+        user = db.query(User).filter_by(email=user_email).first()
 
-        if user_guess == secret_number:
+        if user_guess == user.secret_number:
             text = "Congratulations your are correct."
             play_again = "Start over"
             new_secret = random.randint(1, 30)
@@ -49,16 +48,16 @@ def game():
 
             return response
 
-        elif user_guess < secret_number:
+        elif user_guess < user.secret_number:
             text = "Sorry your guess is to small."
             play_again = "Play again"
             return render_template("result.html", text=text, play_again=play_again)
 
-        elif user_guess > secret_number:
+        elif user_guess > user.secret_number:
             text = "Sorry your guess is to big."
             play_again = "Play again"
             return render_template("result.html", text=text, play_again=play_again)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(use_reloader=True)
--- models.py
+++ models.py
@@ -1,11 +1,12 @@
 import os
 from sqla_wrapper import SQLAlchemy
 
-db_url = os.getenv("DATABSE_URL", "sqlite:///localhost.sqlite").replace("postgres://", "postgresl://", 1)
+# db = SQLAlchemy("sqlite:///db.sqlite")
+db_url = os.getenv("DATABASE_URL", "sqlite:///db.sqlite").replace("postgres://", "postgresql://", 1)
 db = SQLAlchemy(db_url)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=False )
-    email = db.Column(db.String, unique=False)
-    secret_number = db.Column(db.String, unique=False)
+    name = db.Column(db.String, unique=True )
+    email = db.Column(db.String, unique=True)
+    secret_number = db.Column(db.Integer, unique=False)
